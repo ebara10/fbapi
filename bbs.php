829251982ae105f6e713dfebd73a0d2a6dd884b6
chihiro.ebara <chihiro.ebara@192.168.0.65>
2014-06-26 11:27:05 +0900

    change form

--- bbs.php
+++ bbs.php
@@ -3,11 +3,11 @@
 ?>
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
-
-<head><title>PHP TEST</title></head>
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<head>
+    <title>PHP TEST</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+</head>
 <body>
-
 <?php
     if (isset($_COOKIE["visited"])){
         $count = $_COOKIE["visited"] + 1;
@@ -21,25 +21,24 @@
 <p>PHPのテストです。</p>
 
 <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-名前:<input type="text" name="name" value="test"><br>
-タイトル:<input type="text" name="title" value="test"><br>
-内容:<textarea name="content" rows="4" cols="40" alt="test"></textarea><br>
-<input type="submit" name="btn1" value="送信">
+    名前:<input type="text" name="name" value="test"><br>
+    タイトル:<input type="text" name="title" value="test"><br>
+    <p>内容:</p>
+        <p><textarea name="content" rows="4" cols="40" alt="test"></textarea></p>
+    <input type="submit" name="btn1" value="送信">
 </form>
 
 <?php
-
-$name = $_POST["name"];
-$title = $_POST["title"];
-$content = nl2br($_POST["content"]);
-$post_date = date('y/m/d') + time();
+$postName = $_POST["name"];
+$postTitle = $_POST["title"];
+$postContent = nl2br($_POST["content"]);
+$postDate = date('y/m/d') + time();
 
 print('<p>投稿者:'.$name.'</p>');
 print('<p>タイトル:'.$title.'</p>');
-print('<p>投稿日時:'.$post_date.'</p>');
+print('<p>投稿日時:'.$postDate.'</p>');
 print('<p>内容:</p>');
 print('<p>'.$content.'</p>');
-
 ?>
 
 
