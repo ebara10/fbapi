a66ef68357702ffd35b5a8613ae6dbba52e9f051
“chihiro.ebara” <chihiro.ebara@net-marketing.co.jp>
2014-07-03 19:45:19 +0900

    日付取得追加

--- insert.php
+++ insert.php
@@ -13,16 +13,19 @@ try {
 	die();
 }
 
+//bbs.phpで入力したデータを引き継ぐ
 $name = $_REQUEST["name"];
 $title = $_REQUEST["title"];
 $body = nl2br($_REQUEST["content"]);
 $post_date = date('y/m/d') + time();
 
-//bbs.phpで入力したデータを
+//上の変数にはいっているデータをinsert文に入れる
 $stmt = $pdo->prepare('INSERT INTO topics (title, body, name, post_date) VALUES (:title, :body, :name, :post_date)');
 $stmt -> bindValue(':title', $title,　PDO::PARAM_STR);
 $stmt -> bindValue(':body', $body,　PDO::PARAM_STR);
 $stmt -> bindValue(':name', $name,　PDO::PARAM_STR);
+$stmt -> bindValue(':post_date', $post_date,　PDO::PARAM_STR);
+
 //ここで送る
 $stmt->execute();
 
